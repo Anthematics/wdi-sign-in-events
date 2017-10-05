a05b348258bc1c33a72df7814f6fedb94fe0c72c
Anthematics <jasonharder2@gmail.com>
2017-10-05 16:52:57 -0400

    user can open and close sign in window -> all elements have defined variables (that are relevent)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-modal = document.querySelector('.modal').addEventListener('click',haveclicked);
+document.addEventListener('DOMContentLoaded', function() {
 
-$(selector).fadeIn(speed,easing,callback)
+let signin = document.querySelector(".signin") //had to define both modal and sign in button , so we could define a function where we display modal on signin->
+let modal = document.querySelector(".modal")
+let close = document.querySelector(".close")
+let submit =document.querySelector(".submit")
+let username=document.querySelector("#user")
+let password = document.querySelector("#pass")
+
+	signin.addEventListener('click',	function() {
+			modal.style.display = 'block'
+		})
+
+	close.addEventListener('click', function() {
+			modal.style.display = 'none'
+		})
+
+	submit.addEventListener('click',function(){
+
+
+	})
+
+});
